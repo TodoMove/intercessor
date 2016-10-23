c7a06cd277e97d99a93cca956e7f0b3935ba1135
Ashley Hindle <hey@ashleyhindle.com>
2016-10-23 11:13:40 +0100

    Adds 'Metable' trait which allows us to store metadata on folders/projects/tags/taks/repeats

---
+++ src/TodoMove/Intercessor/Traits/Metable.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace TodoMove\Intercessor\Traits;
+
+trait Metable
+{
+    protected $meta = [];
+
+    public function meta($key, $value = null)
+    {
+        if (empty($key)) {
+            Throw new \LogicException('Must pass key');
+        }
+
+        if (!is_string($key) && !is_int($key)) {
+            Throw new \LogicException('Key must be an int or string');
+        }
+
+        if (is_null($value)) { // This does mean you can't store any metadata with the value of null, but "it's a sacrifice I'm willing to make" (Lord Farquaad, Shrek)
+            return $this->meta[$key] ?: null;
+        }
+
+        $this->meta[$key] = $value;
+
+        return $this;
+    }
+}
